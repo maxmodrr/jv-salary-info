63997feba275760f6ad8232e423cfff6ae45151a
maxym.modrytskyi <maxmodrr@gmail.com>
2025-12-26 02:21:52 +0200

    after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,20 +1,27 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String baseMessage = "Report for period " + dateFrom + " - " + dateTo;
-        String[] results = new String[names.length];
-        int dateFromInteger = convertDateToInteger(dateFrom);
-        int dateToInteger = convertDateToInteger(dateTo);
+        StringBuilder builder = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
 
         for (int i = 0; i < names.length; i++) {
             String name = names[i];
             int sum = 0;
             for (String oneData: data) {
                 String[] parts = oneData.split(" ");
-                if (parts[1].equals(name)) {
-                    int employeeDate = convertDateToInteger(parts[0]);
-                    if (employeeDate >= dateFromInteger && employeeDate <= dateToInteger) {
+                String employeeName = parts[1];
+                if (employeeName.equals(name)) {
+                    String employeeStringDate = parts[0];
+                    LocalDate employeeDate = LocalDate.parse(employeeStringDate, formatter);
+                    if (!employeeDate.isAfter(to) && !employeeDate.isBefore(from)) {
                         int hours = Integer.parseInt(parts[2]);
                         int salary = Integer.parseInt(parts[3]);
                         sum += hours * salary;
@@ -22,23 +29,9 @@ public class SalaryInfo {
                 }
 
             }
-            results[i] = name + " - " + sum;
-        }
-        StringBuilder builder = new StringBuilder();
-
-        for (String result: results) {
-            builder.append("\n").append(result);
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(sum);
         }
 
-        return baseMessage + builder.toString();
-    }
-
-    private int convertDateToInteger(String data) {
-        String[] parts = data.split("\\.");
-        int day = Integer.parseInt(parts[0]);
-        int month = Integer.parseInt(parts[1]);
-        int year = Integer.parseInt(parts[2]);
-
-        return year * 10000 + month * 100 + day;
+        return builder.toString();
     }
 }
